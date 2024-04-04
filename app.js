5c9ea048eea74be1314c268f8440764f027e4dc0
Huseyin Elibol <elibolh0@gmail.com>
2024-04-05 02:09:22 +0300

    Template engine added

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
 
 //MIDDLEWARES
 app.use(express.static('public'));
 
-
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('index');
 });
 
 const port = 3000;
